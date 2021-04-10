c40cd9cd2785568c6ab407cbcb9b2d5dae02d8d1
Janu <janu.m.sung@gmail.com>
2021-04-10 14:53:26 -0700

    imgs and text

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,7 +1,29 @@
 import React from "react";
 
 const Home = () => {
-  return <div className="home-cont">I am a home component</div>;
+  return (
+    <div className="home-cont">
+      <div className="section-1">
+        <img
+          src="https://thumbs.dreamstime.com/z/chinese-style-wave-water-element-27088321.jpg"
+          alt="chinese water renderings"
+        />
+        <div className="main epigraph">
+          “You must be shapeless, formless, like water. When you pour water in a
+          cup, it becomes the cup. When you pour water in a bottle, it becomes
+          the bottle. When you pour water in a teapot, it becomes the teapot.
+          Water can drip and it can crash. Become like water my friend.”
+        </div>
+        <img
+          src="https://media.gettyimages.com/photos/kung-fubruce-lee-who-plays-kato-in-abctvs-the-green-hornet-fridays-picture-id517264052?s=2048x2048"
+          alt="Bruce Lee Trip-tyc"
+          width="500"
+          height="600"
+        />
+      </div>
+      I am a home component
+    </div>
+  );
 };
 
 export default Home;
